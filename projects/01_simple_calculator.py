3710e1f2576c0020dd269c2490822b83dcf501bf
Mayur Ichake <mayurichake4@gmail.com>
2025-07-26 22:55:52 +0530

    done

---
+++ projects/01_simple_calculator.py
@@ -0,0 +1,13 @@
+# simple calculator
+
+name = input("Enter your name:",)
+print(f"Hello, {name}!")
+print("Please, Enter your value......")
+a = int(input("Value 1:",))
+b = int(input("Value 2:",))
+# decision = input("What you want....\n")
+print("Addition of two no.is:",a+b)
+print("Subtraction of two no.is:",a-b)
+print("Multiplication of two no.is:",a*b)
+print("Division of two no.is:",a/b)
+print("Reminder of two no.is:",a%b)
